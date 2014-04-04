9b336dbdb0203244282cd9c3421458184a291630
Buck Bailey <r.buck.bailey@gmail.com>
2014-04-04 10:32:54 -0400

    default stage working

--- Stage.java
+++ Stage.java
@@ -35,7 +35,7 @@ public class Stage {
   }
 
   public void draw() {    
-      p.image(backdrops.get(backdropNumber), -backdrops.get(backdropNumber).width, -backdrops.get(backdropNumber).height, backdrops.get(backdropNumber).width,
+      p.image(backdrops.get(backdropNumber), p.width/2, p.height/2, backdrops.get(backdropNumber).width,
         backdrops.get(backdropNumber).height);
   }
 
