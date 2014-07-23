27b77540d2851385aa91e0745a3d49bee54c6e3b
Buck Bailey <r.buck.bailey@gmail.com>
2014-07-23 11:55:58 -0400

    simple sample source java

--- Sprite.java
+++ Sprite.java
@@ -42,15 +42,16 @@ public class Sprite {
   public PVector pos = new PVector(0, 0);
   public float spin = 0;
   public boolean penDown;
-  public float direction = 0;
+  
   /* DIRECTION IS IN DEGREES! any math will require conversion.
    * This for end-user simplicity.
    * Use degrees() to convert to degrees; radians() to convert to
    * radians.
    */
-   
+  public float direction = 0;
   Sprite (PApplet parent) {
     p = parent;
+    loadDefaultCostumes();
     costumeNumber=0;
     visible = true;
     numberOfCostumes=0;
@@ -73,10 +74,14 @@ public class Sprite {
     if (visible) {
       p.pushMatrix(); // save old visual style for other sprites
       // set the center of the screen to (0, 0)
+//      p.translate((p.width/2)+pos.x, (p.height/2)+pos.y);    
       p.translate((p.width/2)+pos.x, (p.height/2)+pos.y);    
-          
+    
+      //p.print("x "); p.print(pos.x); p.print(" y "); p.println(pos.y);
+      
       p.imageMode(p.CENTER);
       // locked left-right rotation
+      //if (((direction<=359) & (direction>=180)) & rotationStyle==rotationStyle_LeftRight) p.scale(-1.0f,1.0f);
       if (((direction<=359) & (direction>90)) & rotationStyle==rotationStyle_LeftRight) p.scale(-1.0f,1.0f);
       if (rotationStyle==rotationStyle_AllAround) p.rotate(p.radians(-direction));
       if (ghostEffect < 255) {
--- Stage.java
+++ Stage.java
@@ -35,24 +35,21 @@ public class Stage {
   // 
   // Backdrop 0 should always the X/Y grid, for debugging movement
   public static final int bg_grid=0;
+  public static final int bg_title=1;
+  public static final int bg_highway=2;
+  public static final int bg_gameover=3;
 
   public int startTime;
   public int backdropNumber, numberOfBackdrops;
   public ArrayList<PImage> backdrops = new ArrayList<PImage>();
-  boolean askingQuestion = false;
-  String question = "What is your quest?";
-  String questionText = "";
-  String theAnswer = "";
-  PFont questionFont;
- 
+  
   Stage (PApplet parent) {
     p = parent;
     backdropNumber=0;
     numberOfBackdrops=0;
     startTime=0;
     resetTimer();
-    questionFont = p.createFont("Helvetica", 18); 
-    p.textFont(questionFont,18);
+    loadDefaultBackdrops();
   }
   
   // the timer returns seconds, in whole numbers (integer)
@@ -61,66 +58,20 @@ public class Stage {
     return temp-startTime;
   } 
   
+  // reset the stage timer
   public void resetTimer() {
     startTime = p.millis()/1000;
   }
 
+
   public void update() {
-    p.translate((p.width/2),(p.height/2));    
+//    p.translate(-(p.width/2), -(p.height/2));
     draw();    
-    if (askingQuestion) drawQuestionText(); // ask(question);
   }
 
   public void draw() {    
-        p.image(backdrops.get(backdropNumber), -p.width/2, -p.height/2, backdrops.get(backdropNumber).width,
+        p.image(backdrops.get(backdropNumber), p.width/2, p.height/2, backdrops.get(backdropNumber).width,
         backdrops.get(backdropNumber).height);
-  }
-
-  public void questionKeycheck() {
-     if (p.key != p.CODED) {
-     if (p.key==p.BACKSPACE)
-        questionText = questionText.substring(0,p.max(0,questionText.length()-1));
-     else if (p.key==p.TAB)
-        questionText += "    ";
-     else if (p.key==p.ENTER|p.key==p.RETURN) {
-        theAnswer = questionText;
-        questionText="";
-        askingQuestion = false;
-     }
-     else if (p.key==p.ESC|p.key==p.DELETE) {
-     }
-     else questionText += p.key;
-    }
-  }
-
-  public String answer() {
-    String finalResponse;
-    if (theAnswer!="") { 
-      finalResponse=theAnswer; 
-      theAnswer = ""; 
-      return finalResponse; 
-    } 
-    else return "";
-  }
-
-  public void ask(String newQuestion) {
-    drawQuestionText();
-    String theAnswer = "";
-    askingQuestion = true;
-    question = newQuestion;
-  }
-
-  public void drawQuestionText() {
-    p.pushStyle();
-    p.stroke(0);
-    p.fill(0,125,175);
-    p.rect(-220,110,440,50,15);
-    p.fill(255);
-    p.rect(-217,113,434,44,15);
-    p.fill(0,0,0);
-    p.textFont(questionFont,18);
-    p.text(question+" "+questionText+(p.frameCount/10 % 2 == 0 ? "_" : ""), -210, 143);
-    p.popStyle();
   }
 
   // load xy grid as backdrop 0
@@ -139,17 +90,20 @@ public class Stage {
   public void nextBackdrop() { 
     backdropNumber++;
     if (backdropNumber > numberOfBackdrops + 1) backdropNumber=0;
+    draw();
   }
 
   // change to previous backdrop
   public void previousCostume() {
     backdropNumber--;
     if (backdropNumber < 0) backdropNumber=backdropNumber;
+    draw();
   }
 
   // switch to specific costume
   public void switchToBackdrop(int newBackdropNumber) {
     backdropNumber=newBackdropNumber;
+    draw();
   }
 
   }
